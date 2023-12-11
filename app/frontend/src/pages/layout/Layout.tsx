242025141ed9f74b75af4c646de19a0022c5a164
Adnan Ashraf <adnan.newton@gmail.com>
2023-12-11 17:55:17 +0500

    fixed

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -30,16 +30,14 @@ const Layout = () => {
                                     Ask a question
                                 </NavLink>
                             </li>
-                            <li className={styles.headerNavLeftMargin}>
-                                <a href="https://example.com" target={"_blank"} title="Example.com link">
-                                    <img src={logo} alt="Logo" aria-label="Link to example.com" width="20px" height="20px" className={styles.logo} />
-                                </a>
-                            </li>
                         </ul>
                     </nav>
                     <h4 className={styles.headerRightText} onClick={() => (window.location.href = "https://example.com")}>
                         Powered by Example.com
                     </h4>
+                    <a href="https://example.com" target={"_blank"} title="Example.com link">
+                        <img src={logo} alt="Logo" aria-label="Link to example.com" width="20px" height="20px" className={styles.logo} />
+                    </a>
                     {useLogin && <LoginButton />}
                 </div>
             </header>
